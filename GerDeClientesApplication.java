4bbe88d6ea5636a4d9d24cebb0319e5281939843
julio20212 <162452155+Julio20212@users.noreply.github.com>
2025-04-01 14:39:42 -0300

    Update GerDeClientesApplication.java

--- GerDeClientesApplication.java
+++ GerDeClientesApplication.java
@@ -1,13 +1,8 @@
-package com.GerDeClientes;
+spring.application.name=GerDeClientes
+spring.datasource.url=jdbc;mysql://localhost:3306/clientes
+spring.datasource.driver-class-name=com.mysql.cj.jdbc.Driver
+spring.datasource.username=root
+spring.datasource.password=P@ssw0rd
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class GerDeClientesApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GerDeClientesApplication.class, args);
-	}
-
-}
+spring.jpa.database=mysql
+spring.jpa.show-sql=true
